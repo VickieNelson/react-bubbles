b74ebc3b2697410391e569604a523c49c493bae2
Vickie Nelson <vickie.isabel.nelson@gmail.com>
2020-06-19 10:52:56 -0500

    private route coded

--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -1,19 +1,31 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
+import { axiosWithAuth } from "../utils/axiosWithAuth";
 
 import Bubbles from "./Bubbles";
 import ColorList from "./ColorList";
 
-const BubblePage = () => {
+const BubblePage = (props) => {
   const [colorList, setColorList] = useState([]);
-  // fetch your colors data from the server when the component mounts
-  // set that data to the colorList state property
+
+  useEffect(() => {
+    // fetch your colors data from the server when the component mounts
+    // set that data to the colorList state property
+    axiosWithAuth()
+      .get("/api/colors")
+      .then((res) => {
+        setColorList(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+        alert("Error fetching colors. PLease try again.", props);
+      });
+  }, [props]);
 
   return (
-    <>
+    <div className='bubble-page-style'>
       <ColorList colors={colorList} updateColors={setColorList} />
       <Bubbles colors={colorList} />
-    </>
+    </div>
   );
 };
 
